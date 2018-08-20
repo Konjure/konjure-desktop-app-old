abce3aac3250a3eb7723b0bfb433b1cf670469a7
Connor Hollasch <connor@hollasch.net>
2018-08-19 17:39:50 -0700

    Splash screen page!

--- main.js
+++ main.js
@@ -1,50 +1,73 @@
 // Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
+const {app, BrowserWindow} = require('electron');
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-let mainWindow
+let mainWindow;
+let splashWindow;
 
-function createWindow () {
-  // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600})
+function createWindow() {
+    // Create the browser window.
+    mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        icon: 'assets/media/images/favicon.png',
+        show: false,
+    });
 
-  // and load the index.html of the app.
-  mainWindow.loadFile('index.html')
+    // Create the splash screen window.
+    splashWindow = new BrowserWindow({
+        width: 340,
+        height: 510,
+        transparent: true,
+        frame: false,
+        alwaysOnTop: true,
+        movable: false,
+        center: true,
+        skipTaskbar: true,
+    });
 
-  // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
+    // Load splash window.
+    splashWindow.loadURL(`file://${__dirname}/splash.html`);
 
-  // Emitted when the window is closed.
-  mainWindow.on('closed', function () {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    mainWindow = null
-  })
+    // and load the index.html of the app.
+    mainWindow.loadURL(`file://${__dirname}/index.html`);
+
+    mainWindow.on('ready-to-show', function () {
+        splashWindow.destroy();
+        mainWindow.show();
+    });
+
+    // Emitted when the window is closed.
+    mainWindow.on('closed', function () {
+        // Dereference the window object, usually you would store windows
+        // in an array if your app supports multi windows, this is the time
+        // when you should delete the corresponding element.
+        mainWindow = null;
+    })
 }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
+app.on('ready', createWindow);
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function () {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
-})
+    // On OS X it is common for applications and their menu bar
+    // to stay active until the user quits explicitly with Cmd + Q
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
 
 app.on('activate', function () {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) {
-    createWindow()
-  }
-})
+    // On OS X it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (mainWindow === null) {
+        createWindow();
+    }
+});
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
